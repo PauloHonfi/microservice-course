83463cd78e64c7d6b0aa198ba30e54488f5b34e8
Paulo Honfi <contato@paulohonfi.com.br>
2021-07-04 15:16:59 -0300

    Logi, JWT Token

--- hr-oauth/src/main/java/br/com/paulohonfi/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/paulohonfi/hroauth/services/UserService.java
@@ -6,6 +6,9 @@ package br.com.paulohonfi.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.paulohonfi.hroauth.entities.User;
@@ -18,7 +21,7 @@ import br.com.paulohonfi.hroauth.feignclients.UserFeignClient;
  * @Since 06-2021
  */
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -33,6 +36,18 @@ public class UserService {
 		}
 		
 		logger.info("Email found " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("Email not found " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		logger.info("Email found " + username);
 		return user;
 	}
 }
