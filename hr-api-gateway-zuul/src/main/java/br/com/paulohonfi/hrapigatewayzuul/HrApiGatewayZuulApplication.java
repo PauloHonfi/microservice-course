e6539a89f894bc9519e06210d3f192ef38521313
Paulo Honfi <contato@paulohonfi.com.br>
2021-07-04 16:29:30 -0300

    zull authorization

--- hr-api-gateway-zuul/src/main/java/br/com/paulohonfi/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/br/com/paulohonfi/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,4 +1,4 @@
-package br.com.paulohonfi.hrpayroll.hrapigatewayzuul;
+package br.com.paulohonfi.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
